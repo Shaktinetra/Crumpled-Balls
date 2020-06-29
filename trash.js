374312099071e83570bf38b0bde83376856cf628
Shakti <65832818+Shaktinetra@users.noreply.github.com>
2020-06-29 14:23:18 -0400

    Update trash.js

--- trash.js
+++ trash.js
@@ -6,14 +6,16 @@ class Trash {
         this.bodyb = Bodies.rectangle(600, 570, 100, 10, options);
         this.bodyr = Bodies.rectangle(635, 565, 10, 100, options);
         this.bodyl = Bodies.rectangle(565, 565, 10, 100, options);
-        this.image = loadImage("dustbingreen.png");
         World.add(world, this.bodyb);
         World.add(world, this.bodyr);
         World.add(world, this.bodyl);
     }
 
     display() {
-        imageMode(CENTER);
-        image(this.image, 600, 560, 150, 150)
+        rectMode(CENTER);
+        
+        rect(600, 570, 100, 10);
+        rect(635, 565, 10, 100);
+        rect(565, 565, 10, 100);
     }
 }

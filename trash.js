df0b0d830cf754e4d17922c16321c29c6385b220
Shakti <65832818+Shaktinetra@users.noreply.github.com>
2020-07-01 13:41:06 -0400

    Update trash.js

--- trash.js
+++ trash.js
@@ -3,7 +3,7 @@ class Trash {
         var options ={
             isStatic: true
         }
-        this.bodyb = Bodies.rectangle(600, 625, 100, 10, options);
+        this.bodyb = Bodies.rectangle(600, 600, 100, 10, options);
         this.bodyr = Bodies.rectangle(635, 565, 10, 100, options);
         this.bodyl = Bodies.rectangle(565, 565, 10, 100, options);
         World.add(world, this.bodyb);
@@ -14,7 +14,7 @@ class Trash {
     display() {
         rectMode(CENTER);
         
-        rect(600, 625, 100, 10);
+        rect(600, 600, 100, 10);
         rect(635, 565, 10, 100);
         rect(565, 565, 10, 100);
     }
